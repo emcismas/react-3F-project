ea38c573a05d3be084c7e9114fad9b4ad89c4ccd
em-cismas <cismasemanuela02@gmail.com>
2024-09-01 22:15:24 +0300

    Moved project

---
+++ react-portfolio/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      <NavLink
+        to="/"
+        className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
+      >
+        <p className="purple-gradient_text">EM</p>
+      </NavLink>
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink to="/about">About</NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
