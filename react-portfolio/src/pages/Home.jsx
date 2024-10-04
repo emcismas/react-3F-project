18826bf127ae369b27dce551ee0ed39088d8b02f
em-cismas <cismasemanuela02@gmail.com>
2024-10-04 23:32:10 +0300

    Created pop-up component

--- react-portfolio/src/pages/Home.jsx
+++ react-portfolio/src/pages/Home.jsx
@@ -39,6 +39,9 @@ const Home = () => {
 
   return (
     <section className="w-full h-screen relative">
+      <div className="absolute top-28 left-0 right-0 z-10 flex items-center  justify-center">
+        POPUP
+      </div>
       <Canvas
         className={`w-full h-screen bg-transparent ${
           isRotating ? 'cursor-grabbing' : 'cursor-grab'
