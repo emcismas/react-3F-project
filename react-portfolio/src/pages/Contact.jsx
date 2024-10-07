7422f61064f851a67df3abe971de5d8360fb70cf
em-cismas <cismasemanuela02@gmail.com>
2024-10-08 02:21:04 +0300

    Finished Contact Form UI

--- react-portfolio/src/pages/Contact.jsx
+++ react-portfolio/src/pages/Contact.jsx
@@ -1,9 +1,86 @@
-import React from 'react';
-import Sky from '../assets/models/Sky';
-import { Canvas } from '@react-three/fiber';
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
-  return <section className="w-full h-screen relative"></section>;
+  const formRef = useRef(null);
+  const [form, setForm] = useState({ name: '', email: '', message: '' });
+  const [isLoading, setIsLoading] = useState(false);
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleFocus = () => {};
+  const handleBlur = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    emailjs.sendForm();
+  };
+
+  return (
+    <section className=" relative flex lg:flex-row flex-col max-container ">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in Touch </h1>
+        <form
+          className="w-full flex flex-col gap-7 mt-14"
+          onSubmit={handleSubmit}
+        >
+          <label className="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="John Doe"
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Email
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="john.doe@gmail.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <label className="text-black-500 font-semibold">
+            Your Message
+            <input
+              type="text"
+              name="message"
+              className="textarea"
+              placeholder="Let me know more about your project!"
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <button
+            className="btn"
+            text="submit"
+            disabled={isLoading}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+            {isLoading ? 'Sending...' : 'Send Message'}
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
