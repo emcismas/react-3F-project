8dba0545eb361f82d0ad521e0990c248c2c3a575
em-cismas <cismasemanuela02@gmail.com>
2024-10-07 11:41:09 +0300

    Revert "Redid Navbar + Started working on Contact page"
    This reverts commit b33c3d452ce4f9bc4a1270ab756d8b13758d3567.

--- react-portfolio/src/components/Navbar.jsx
+++ react-portfolio/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         <NavLink
           to="/about"
           className={({ isActive }) =>
-            isActive ? 'text-slate-300/50' : 'text-white'
+            isActive ? 'text-purple-500' : 'text-white'
           }
         >
           About Me
@@ -22,7 +22,7 @@ const Navbar = () => {
         <NavLink
           to="/contact"
           className={({ isActive }) =>
-            isActive ? 'text-slate-300/50' : 'text-white'
+            isActive ? 'text-purple-500' : 'text-white'
           }
         >
           Contact
